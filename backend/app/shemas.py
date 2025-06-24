d310dffa9cc3e06093a98480f7c85e63de7fa678
aleksOtryshko <55054459+aleksOtryshko@users.noreply.github.com>
2025-06-24 14:35:07 +0300

    Update shemas.py

--- backend/app/shemas.py
+++ backend/app/shemas.py
@@ -1,24 +1,11 @@
 from pydantic import BaseModel
-from enum import Enum
-from datetime import datetime
-
-class OrderStatusEnum(str, Enum):
-    pending = "pending"
-    in_progress = "in_progress"
-    completed = "completed"
-    cancelled = "cancelled"
 
 class OrderCreate(BaseModel):
     phone_number: str
     name: str
     problem_description: str
 
-class OrderProgressCreate(BaseModel):
-    status: OrderStatusEnum
-    order_id: int
-    master_id: int
-    comment: str
-
-class MasterCreate(BaseModel):
-    telegram_id: str
-    name: str
+class OrderOut(OrderCreate):
+    id: int
+    status: str
+    created_at: str
