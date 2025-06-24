8da7fafe3ad523babf972b2a2a75664ccd75e7ff
aleksOtryshko <55054459+aleksOtryshko@users.noreply.github.com>
2025-06-24 14:41:35 +0300

    Create config.py

---
+++ telegram_bot/config.py
@@ -0,0 +1,6 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
