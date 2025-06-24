4bf4e2e4ddab6466ada6d875d8fe9bb2e96cdc67
aleksOtryshko <55054459+aleksOtryshko@users.noreply.github.com>
2025-06-24 14:48:26 +0300

    Create bot.py

---
+++ telegram_bot/bot.py
@@ -0,0 +1,13 @@
+import asyncio
+from telegram.ext import ApplicationBuilder, CallbackQueryHandler
+import config, handlers
+
+async def main():
+    app = ApplicationBuilder().token(config.TELEGRAM_TOKEN).build()
+    app.add_handler(CallbackQueryHandler(handlers.button_callback))
+    # раз в 30 сек. шлём новые заказы
+    app.job_queue.run_repeating(handlers.notify_masters, interval=30, first=10)
+    await app.run_polling()
+
+if __name__=="__main__":
+    asyncio.run(main())
