bb3f7b24ac64f5ed8c20e5ee50bb0fcf3e1c91e6
aleksOtryshko <55054459+aleksOtryshko@users.noreply.github.com>
2025-07-26 11:25:41 +0300

    Update config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,5 +1,14 @@
 import os
 from dotenv import load_dotenv
+
+# Загружаем локальный .env (если запускается локально)
 load_dotenv()
+
+# Если существует секретный файл от Render — загружаем его тоже
+secret_env_path = "/etc/secrets/.env"
+if os.path.exists(secret_env_path):
+    load_dotenv(dotenv_path=secret_env_path)
+
+# Теперь переменные будут доступны из окружения
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
